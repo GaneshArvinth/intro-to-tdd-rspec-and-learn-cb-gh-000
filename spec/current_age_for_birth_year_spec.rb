7359d82a0489e60fccc710ee0845175e2c8dd82f
Ganesh Arvinth <ganesharvinth0@gmail.com>
2018-04-16 12:22:38 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -3,7 +3,7 @@ require_relative "../current_age_for_birth_year.rb"
 describe "Calculate age method" do
   it "from the current year" do
     currentYear = Time.now.year
-    birthYear = gets.to_i
+    birthYear = 1992
 
     currentAge = currentYear - birthYear
     actualAge = calaculateAge(birthYear)
