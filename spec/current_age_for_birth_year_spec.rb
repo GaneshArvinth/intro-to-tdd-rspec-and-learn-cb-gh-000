758a648ff989190ca6a4db89b7fa74f94ce56de2
Ganesh Arvinth <ganesharvinth0@gmail.com>
2018-04-16 12:21:22 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,6 +1,6 @@
 require_relative "../current_age_for_birth_year.rb"
 
-require "Calculate age method" do
+describe "Calculate age method" do
   it "from the current year" do
     currentYear = Time.now.year
     birthYear = gets.to_i
